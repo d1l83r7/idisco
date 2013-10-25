18718ab87194f6e6e32928d148191a6bb6b5c7d4
dilbert88 <dilbert88@aff060bc-a715-77b3-fc92-35b26f17b861>
2013-10-25 16:03:10 +0000

    Servicio discotecas por proximidad

---
+++ trunk/uparty_backend/app/controllers/GetDiscotecasCercanas.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.DiscotecaDTO;
+import play.mvc.Controller;
+
+import com.google.gson.JsonArray;
+
+import es.uparty.utils.Utils;
+
+public class GetDiscotecasCercanas extends DiscotecaGenericController {
+	public static void getDiscotecasCercanas(String distancia, String latitud, String longitud){
+		String sql = "SELECT * FROM DISCOTECAS";
+		List<DiscotecaDTO> l = ejecutarSQL(sql);
+		double longOrigen = Double.parseDouble(longitud);
+		double latOrigen = Double.parseDouble(latitud);
+		
+		List<DiscotecaDTO> lDiscotecasCercanas = new ArrayList<DiscotecaDTO>();
+		for(DiscotecaDTO dto: l){
+			double dist = Utils.calculaDistancia(latOrigen, longOrigen, dto.getLatitud(), dto.getLongitud());
+			Double d = Double.valueOf(dist)*1000;
+			if(d.intValue()<=Integer.parseInt(distancia)){
+				lDiscotecasCercanas.add(dto);
+			}
+		}
+		
+		JsonArray array = listDicotecaDTOToJSonArray(lDiscotecasCercanas);
+		renderJSON(array);
+	}
+
+}
