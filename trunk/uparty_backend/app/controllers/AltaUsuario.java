a0b90aab0ec11278f026ad726a0c64ebab3e07ba
dilbert88 <dilbert88@aff060bc-a715-77b3-fc92-35b26f17b861>
2013-11-01 00:52:28 +0000

    gestión de usuarios

---
+++ trunk/uparty_backend/app/controllers/AltaUsuario.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import java.sql.SQLException;
+
+import models.User;
+
+public class AltaUsuario extends UsuarioGenericController {
+	public static void insertarUsuario(String usuario, String password,
+			String perfil){
+		usuarioLogado();
+		User u = new User();
+		u.setUsuario(usuario);
+		u.setPassword(password);
+		u.setPerfil(perfil);
+		boolean res = false;
+		try{
+			res = darAltaUsuario(u);
+		}catch(SQLException sqle){
+			sqle.printStackTrace();
+			res = false;
+		}
+		
+		if(res)
+			render();
+		else
+			errorAltaUsuario();
+	}
+	
+	public static void verAltaUsuario(){
+		render();
+	}
+	
+	public static void errorAltaUsuario(){
+		render();
+	}
+}
---
+++ trunk/uparty_backend/app/controllers/GestionDiscoteca.java
@@ -0,0 +1,73 @@
+package controllers;
+
+import java.util.List;
+
+import models.DiscotecaDTO;
+
+public class GestionDiscoteca extends DiscotecaGenericController {
+	public static void gestionDiscoteca(){
+		usuarioLogado();
+		render();		
+	}
+	
+	public static void verGestionDiscoteca(String name){
+		usuarioLogado();
+		String sql = "SELECT * FROM DISCOTECAS WHERE LOWER(NOMBRE) LIKE '%"+name.toLowerCase()+"%'";
+		List<DiscotecaDTO> l = seleccionarDiscotecas(sql);
+		render(l);
+	}
+	
+	public static void eliminaDiscoteca(String idDiscoteca){
+		usuarioLogado();
+		boolean res = borrarDiscoteca(idDiscoteca);
+		if(res)
+			render();
+		else
+			errorEliminaDiscoteca();
+	}
+	
+	public static void errorEliminaDiscoteca(){
+		usuarioLogado();
+		render();
+	}
+	
+	public static void editaDiscoteca(String idDiscoteca){
+		usuarioLogado();
+		String sql = "select * from public.\"discotecas\" where discotecas.\"idDiscoteca\"="+idDiscoteca;
+		List<DiscotecaDTO> l = seleccionarDiscotecas(sql);
+		if(l.size()>0){
+			DiscotecaDTO discotecaDTO = l.get(0);
+			render(discotecaDTO);
+		}else{
+			errorEditaDiscoteca();
+		}
+	}
+	
+	public static void modificaDiscoteca(String idDiscoteca, String nombre,
+			String descripcion, String latitud, String longitud){
+		usuarioLogado();
+		DiscotecaDTO dto = new DiscotecaDTO();
+		dto.setNombre(nombre);
+		dto.setDescripcion(descripcion);
+		dto.setLatitud(Double.parseDouble(latitud));
+		dto.setLongitud(Double.parseDouble(longitud));
+		dto.setIdDiscoteca(Long.parseLong(idDiscoteca));
+		
+		boolean res = modificarDiscoteca(dto);
+		if(res)
+			render();
+		else
+			errorModificaDiscoteca();
+		
+	}
+	
+	public static void errorModificaDiscoteca(){
+		usuarioLogado();
+		render();
+	}
+	
+	public static void errorEditaDiscoteca(){
+		usuarioLogado();
+		render();
+	}
+}
