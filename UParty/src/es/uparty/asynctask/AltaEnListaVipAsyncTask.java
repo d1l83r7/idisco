57445b9f4f0378014e1669d3d870e8c29286fdfb
dilbert88 <dilbert88@aff060bc-a715-77b3-fc92-35b26f17b861>
2014-01-03 22:42:16 +0000

    Alta en lista VIP

---
+++ UParty/src/es/uparty/asynctask/AltaEnListaVipAsyncTask.java
@@ -0,0 +1,69 @@
+package es.uparty.asynctask;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import es.uparty.comunes.Utils;
+import es.uparty.dto.DiscotecaDTO;
+import es.uparty.dto.MensajeDTO;
+
+public class AltaEnListaVipAsyncTask extends AsyncTask<String, Integer, String> {
+
+	private static final String AltaEnListaVipAsyncTask_TAG = "AltaEnListaVipAsyncTask_TAG";
+	
+	@Override
+	protected String doInBackground(String... params) {
+		try{
+			StringBuilder sb = new StringBuilder();
+			sb.append("http://radiant-ravine-3483.herokuapp.com/altaEnListaVIP?");
+			sb.append("usuario="+Utils.encryptURL(params[0]));
+			sb.append("&password="+Utils.encryptURL(params[1]));
+			sb.append("&idDiscoteca="+params[2]);
+			sb.append("&acompanyantes="+params[3]);
+			String res = abrirConexion(sb.toString());
+			return res;
+		}catch(Exception e){
+			Log.d(AltaEnListaVipAsyncTask_TAG, "Error: "+e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private String abrirConexion(String url){
+		try{
+			HttpPost httppost = new HttpPost(url);
+			HttpClient httpclient = new DefaultHttpClient();
+	
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			InputStream is = null;
+			is = entity.getContent();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+			StringBuilder sb = new StringBuilder();
+			sb.append(reader.readLine() + "\n");
+			String line = "0";
+			while ((line = reader.readLine()) != null) {
+			    sb.append(line + "\n");
+			}
+			is.close();
+			reader.close();
+			String result = sb.toString();
+			JSONObject jsonObject = new JSONObject(result);
+			return "S";
+		}catch(Exception e){
+			Log.d(AltaEnListaVipAsyncTask_TAG, "Error: "+e.getMessage());
+			return null;
+		}
+	}
+}
